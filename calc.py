2550001c0517492baaf492c177638f488f062f68
tsusanto1 <tsusanto1@babson.edu>
2017-09-01 20:59:56 -0400

    Session 1
    Session 1 exercise

---
+++ calc.py
@@ -0,0 +1,12 @@
+Exercise 1
+1. Syntax Error: Missing parantheses
+2. NameError: name is not defined
+3. It reads like a normal addition (2++2= 4)
+4. Syntax Error: invalid token
+5. It reads as a single number
+
+Exercise 2
+print (42*60+42)
+print (1.61*10)
+print (2562/(10/1.61)/60)
+print ((10/1.61)/2562*60*60)
