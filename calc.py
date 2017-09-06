5148353a3bbf16f0bf82e803ab38f4acad5ce71a
tsusanto1 <tsusanto1@babson.edu>
2017-09-06 19:56:21 -0400

    Tues, Aug 2
    session 2 in class

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+#Thursday, 30 Aug
 Exercise 1
 1. Syntax Error: Missing parantheses
 2. NameError: name is not defined
@@ -10,3 +11,20 @@ print (42*60+42)
 print (1.61*10)
 print (2562/(10/1.61)/60)
 print ((10/1.61)/2562*60*60)
+
+
+# Tuesday, Sept 5
+π = 3.1415926
+print((4/3)*π*5**3)
+
+print((24.95*0.6)*60+3+(0.75*59))
+
+import math
+
+easyP = 8.25
+fastP = 7.2
+startTimeHr = 6.0
+startTimeMin = 52.0
+print( math.floor(startTimeHr+(startTimeMin + easyP*2.0 + fastP*3.0)/60.0), " hour(s) and",  (startTimeMin + easyP*2.0 + fastP*3.0)%60.0, " minute(s).")
+
+print("%.1f" % ((89-82)/82*100))
